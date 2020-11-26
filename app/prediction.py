9be0c9637aab8b5f54325518a9be973f54535688
Gang Lu <lgstone@hotmail.com>
2020-11-27 10:12:57 +1300

    remove file after use

--- app/prediction.py
+++ app/prediction.py
@@ -4,14 +4,14 @@ import os
 
 threshold = 0
 def image_prediction(image_file="2.jpg", model_file="resnet50_weights_tf_dim_ordering_tf_kernels.h5"):
-  execution_path = os.getcwd() + '/app'
+  execution_path = os.getcwd()
 
   prediction = ImagePrediction()
   prediction.setModelTypeAsResNet()
   prediction.setModelPath(os.path.join(execution_path, model_file))
   prediction.loadModel()
 
-  predictions, probabilities = prediction.predictImage(os.path.join(execution_path, image_file), result_count=3, input_type='stream')
+  predictions, probabilities = prediction.predictImage(os.path.join(execution_path, image_file), result_count=3, input_type='file')
   result = []
   for eachPrediction, eachProbability in zip(predictions, probabilities):
     if eachProbability > threshold: 
@@ -30,7 +30,6 @@ def image_prediction_by_url(image_url, model_file="resnet50_weights_tf_dim_order
   with open(filename, "wb") as local_file:
     local_file.write(f.read())
 
-
   #load prediction model
   execution_path = os.getcwd()
   prediction = ImagePrediction()
@@ -46,5 +45,6 @@ def image_prediction_by_url(image_url, model_file="resnet50_weights_tf_dim_order
         'key' : eachPrediction.replace('_', ' '),
         'probability' : eachProbability
       })
+  os.remove(filename)
   return result
 
